5db64fc2d77dd3804806dd530429f663a4a7f719
suin <suinyeze@gmail.com>
2012-12-06 02:03:15 +0900

    add sample tests

---
+++ src/VendorName/PackageName/Tests/Functional/SampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace VendorName\PackageName\Tests\Functional;
+
+class SampleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSample()
+    {
+        $this->assertTrue(false);
+    }
+}
---
+++ src/VendorName/PackageName/Tests/Join/SampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace VendorName\PackageName\Tests\Join;
+
+class SampleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSample()
+    {
+        $this->assertTrue(false);
+    }
+}
